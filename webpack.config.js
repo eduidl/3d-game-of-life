0d4467699c8d65a0efd1b4d6fcd23e5e4ca22fa9
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2018-09-06 23:56:25 +0900

    github pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve('src/index.js'),
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'main.js'
   },
 
