77101e8b0cea9ab1e57f0c2ff8f727c94169f73e
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2019-12-29 01:06:44 +0900

    Recompile

--- src/index.ts
+++ src/index.ts
@@ -9,101 +9,97 @@ const render = (): void => {
   visualizer.render();
 };
 
-const main = (): void => {
-  const settingElm = document.getElementById("setting");
-  if (settingElm == null) {
-    throw TypeError;
-  }
-  settingElm.addEventListener(
-    "mouseover",
-    () => {
-      visualizer.controls.enabled = false;
-    },
-    false
-  );
-  settingElm.addEventListener(
-    "mouseout",
-    () => {
-      visualizer.controls.enabled = true;
-    },
-    false
-  );
+const settingElm = document.getElementById("setting");
+if (settingElm == null) {
+  throw TypeError;
+}
+settingElm.addEventListener(
+  "mouseover",
+  () => {
+    visualizer.controls.enabled = false;
+  },
+  false
+);
+settingElm.addEventListener(
+  "mouseout",
+  () => {
+    visualizer.controls.enabled = true;
+  },
+  false
+);
 
-  let intervalID: number;
-  const startBtn = document.getElementById("start");
-  if (startBtn == null) {
-    throw TypeError;
-  }
-  startBtn.addEventListener(
-    "click",
-    () => {
-      clearInterval(intervalID);
-      intervalID = window.setInterval(() => {
-        if (!lifeGame.updateState()) {
-          clearInterval(intervalID);
-        }
-        visualizer.placeParticles(lifeGame.getCoordinates());
-      }, 250);
-    },
-    false
-  );
-
-  const stopBtn = document.getElementById("stop");
-  if (stopBtn == null) {
-    throw TypeError;
-  }
-  stopBtn.addEventListener(
-    "click",
-    () => {
-      clearInterval(intervalID);
-    },
-    false
-  );
-
-  const oneStepBtn = document.getElementById("oneStep");
-  if (oneStepBtn == null) {
-    throw TypeError;
-  }
-  oneStepBtn.addEventListener(
-    "click",
-    () => {
-      clearInterval(intervalID);
-      lifeGame.updateState();
+let intervalID: number;
+const startBtn = document.getElementById("start");
+if (startBtn == null) {
+  throw TypeError;
+}
+startBtn.addEventListener(
+  "click",
+  () => {
+    clearInterval(intervalID);
+    intervalID = window.setInterval(() => {
+      if (!lifeGame.updateState()) {
+        clearInterval(intervalID);
+      }
       visualizer.placeParticles(lifeGame.getCoordinates());
-    },
-    false
-  );
+    }, 250);
+  },
+  false
+);
 
-  const updateBtn = document.getElementById("update");
-  if (updateBtn == null) {
-    throw TypeError;
-  }
-  updateBtn.addEventListener(
-    "click",
-    () => {
-      clearInterval(intervalID);
-      lifeGame.reset();
-      visualizer.placeParticles(lifeGame.getCoordinates());
-    },
-    false
-  );
+const stopBtn = document.getElementById("stop");
+if (stopBtn == null) {
+  throw TypeError;
+}
+stopBtn.addEventListener(
+  "click",
+  () => {
+    clearInterval(intervalID);
+  },
+  false
+);
 
-  let timer: number;
-  window.addEventListener(
-    "resize",
-    () => {
-      if (timer > 0) clearTimeout(timer);
-      timer = window.setTimeout(() => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-        visualizer.resize(width, height);
-      }, 200);
-    },
-    false
-  );
+const oneStepBtn = document.getElementById("oneStep");
+if (oneStepBtn == null) {
+  throw TypeError;
+}
+oneStepBtn.addEventListener(
+  "click",
+  () => {
+    clearInterval(intervalID);
+    lifeGame.updateState();
+    visualizer.placeParticles(lifeGame.getCoordinates());
+  },
+  false
+);
 
-  visualizer.placeParticles(lifeGame.getCoordinates());
-  render();
-};
+const updateBtn = document.getElementById("update");
+if (updateBtn == null) {
+  throw TypeError;
+}
+updateBtn.addEventListener(
+  "click",
+  () => {
+    clearInterval(intervalID);
+    lifeGame.reset();
+    visualizer.placeParticles(lifeGame.getCoordinates());
+  },
+  false
+);
 
-main();
+let timer: number;
+window.addEventListener(
+  "resize",
+  () => {
+    if (timer > 0) clearTimeout(timer);
+    timer = window.setTimeout(() => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      visualizer.resize(width, height);
+    }, 200);
+  },
+  false
+);
+
+visualizer.placeParticles(lifeGame.getCoordinates());
+render();
